3c1f1dddd09dbab1717b4929b110d70413975aa8
dtalankin <dtalankin@placeiq.com>
2016-11-12 23:35:37 +0700

    some issues done

--- src/main/java/net/dtalankin/App.java
+++ src/main/java/net/dtalankin/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.print("Я думаю, ");
+        System.out.print("быть программистом - ");
+        System.out.println("это круто");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("3016");
+        String name;
+        int age;
+        String city;
+        System.out.println("Это не баг, это фича.");
     }
 }
